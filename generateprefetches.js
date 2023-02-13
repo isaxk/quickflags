ff28c7618f93d3097a9038d629e9db8532f2e030
Isaac Boorman <iceboorman@icloud.com>
2023-02-13 09:12:50 +0000

    push

--- generateprefetches.js
+++
@@ -1,8 +0,0 @@
-const countries = require("countries.js");
-
-for(i=0;i<countries.length;i++) {
-    var countryCode = countries[i].Code;
-    var path  = "/flagz/assets/"+countryCode+".svg";
-    var output = "<link rel='prefetch' href='"+ path +"' />"
-    console.log(output)
-}
