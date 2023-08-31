d13b4c0ffd86410a1356e03ac00a14d3bb84d533
isaacboor <iceboorman@gmail.com>
2023-08-31 09:14:42 +0100

    enviroment variables

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCDeBiCu2q9_RRurOI-AtgNh3LN-QE25EA",
-    authDomain: "flagzisaacboor.firebaseapp.com",
-    projectId: "flagzisaacboor",
-    storageBucket: "flagzisaacboor.appspot.com",
-    messagingSenderId: "133359321350",
-    appId: "1:133359321350:web:5edf48926518a8ddc19e87",
-    measurementId: "G-KEPZWD43M0"
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementId
 }
