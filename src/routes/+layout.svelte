a536878f73f437e1a7c49e234d5c7378ced69e0c
isaacboor <iceboorman@gmail.com>
2023-09-01 12:57:05 +0100

    create basic homepage

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+    import '@picocss/pico'
+    import "./app.css"
+</script>
+
+
+<div class="app">
+    <slot/>
+</div>
+
+
+<footer>
+
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+
+</script>
+
+
+<header>
+    <h3>FlameFlags</h3>
+</header>
+
+<main>
+    <div class="play-hero">
+        <button class="play-button">Play</button>
+    </div>
+    
+</main>
+
+<style>
+    .play-button {
+        display: inline;
+        width: 100px
+    }
+    .play-hero {
+        width: 100%;
+        height: 300px;
+        display: grid;
+        place-items: center;
+    }
+</style>
