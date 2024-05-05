e3ed3d76d7967f5248b66dad38c2e3babfc8ac57
Isaac <iceboorman@gmail.com>
2024-05-05 21:31:10 +0100

    smoother page transition

--- src/lib/components/PageTransition.svelte
+++ src/lib/components/PageTransition.svelte
@@ -6,8 +6,8 @@
 
 {#key data.url}
 	<div
-		in:scale={{ delay: 300, duration: 200, start: 0.992, opacity: 0 }}
-		out:scale={{ duration: 200, start: 1.02, opacity: 0 }}
+		in:scale={{ delay: 350, duration: 350, start: 0.992, opacity: 0 }}
+		out:scale={{ duration: 350, start: 1.02, opacity: 0 }}
 		class="h-full"
 	>
 		<slot />
