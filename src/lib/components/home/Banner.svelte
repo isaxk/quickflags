ba6e39b83658e3f13f74771f58c3d54d2d4035ce
Isaac <iceboorman@gmail.com>
2024-05-11 19:32:55 +0100

    darkend banner and made it full width

--- src/lib/components/home/Banner.svelte
+++ src/lib/components/home/Banner.svelte
@@ -2,7 +2,10 @@
     export let href: string = "#";
 </script>
 
-<a {href} class="w-full grid place-content-center h-16 bg-teal-900">
+<a
+    {href}
+    class="fixed left-0 w-full grid place-content-center h-14 bg-teal-900 bg-opacity-65"
+>
     <div>
         <slot />
     </div>
