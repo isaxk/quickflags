1834a791726e3dd0b689d62cf0873996bf717269
Isaac <76838845+isaxk@users.noreply.github.com>
2024-06-02 16:36:33 +0100

    Adjust autocomplete padding

--- src/lib/components/game/input/AutoComplete.svelte
+++ src/lib/components/game/input/AutoComplete.svelte
@@ -1,5 +1,5 @@
 <div
-    class="absolute left-0 bottom-14 md:bottom-20 mb-2 backdrop-blur-sm bg-neutral-950 bg-opacity-40 rounded-md h-max w-full flex flex-col-reverse"
+    class="absolute left-0 bottom-14 md:bottom-16 mb-1 md:mb-3 backdrop-blur-sm bg-neutral-950 bg-opacity-40 rounded-md h-max w-full flex flex-col-reverse"
 >
     <slot />
 </div>
